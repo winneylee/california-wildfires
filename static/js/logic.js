61b67afdedce22f7d7be018cbf3c3f95f25fc2d7
Wendy Mudzinski <wendymud@gmail.com>
2021-09-23 23:01:29 -0700

    fixed code for fire.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -18,42 +18,11 @@ let UCB = L.circleMarker([37.871666, -122.272781], {
   radius: 70
 }).addTo(map);
 
-// Loop through the fires array and create one marker for each fire.
-fires.forEach(function(fires) {
-  console.log(fires)
-  L.marker(Latitude, Lng).addTo(map);
+// Get data from fire.js
+let fireData = fire;
+
+// Loop through the fire array and create one marker for each event.
+fireData.forEach(function(fire) {
+  console.log(fire)
+  L.marker(fire.coordinates).addTo(map);
 });
-
-// // Create a base layer that holds both maps.
-// let baseMaps = {
-//   Street: streets,
-//   Dark: dark
-// };
-
-// // Create the map object with center, zoom level and default layer.
-// let map = L.map('mapid', {
-//   center: [30, 30],
-//   zoom: 2,
-//   layers: [streets]
-// });
-
-// // Pass our map layers into our layers control and add the layers control to the map.
-// L.control.layers(baseMaps).addTo(map);
-
-// // Then we add our 'streets' tile layer to the map.
-// streets.addTo(map);
-
-// // Accessing the County Fire Lines GeoJSON URL
-// let stationData = "https://services1.arcgis.com/jUJYIo9tSA7EHvfZ/arcgis/rest/services/California_County_Boundaries/FeatureServer/0?f=pjson";
-
-// // Grabbing our GeoJSON data.
-// d3.json(stationData).then(function(data) {
-//   console.log(data);
-  
-// // Creating a GeoJSON layer with the retrieved data.
-// L.geoJson(data, {
-//   onEachFeature: function (feature, layer) {
-//     layer.bindPopup("<h1>" + feature.properties.faa + "</h1> <hr> <h2>" + feature.properties.name + "</h2>");
-//   }
-//   }).addTo(map)
-// });
